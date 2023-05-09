44c9c339f6f9589a6a1a1a5c6d7366d75e6d9015
ABASTIU <Anton_Bastiuchenko@epam.com>
2023-05-10 00:00:23 +0300

    move to WPF for device

---
+++ IoT.Backend.Wpf/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace IoT.Device.Wpf
+{
+    internal static class Helper
+    {
+        public static X509Certificate2 LoadProvisioningCertificate(string certificateName, string certificatePrivateKeyName, string certPassword)
+        {
+            var workingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var certificate = Path.Combine(workingDirectory, certificateName);
+            var certificatePrivateKey = Path.Combine(workingDirectory, certificatePrivateKeyName);
+
+            var provisioningCert = File.ReadAllText(certificate);
+            var provisioningKey = File.ReadAllText(certificatePrivateKey);
+
+            var sslCert = X509Certificate2.CreateFromEncryptedPem(provisioningCert, provisioningKey, certPassword);
+            var _provisioningCertificate = new X509Certificate2(sslCert.Export(X509ContentType.Pkcs12));
+
+            return _provisioningCertificate;
+        }
+    }
+}
